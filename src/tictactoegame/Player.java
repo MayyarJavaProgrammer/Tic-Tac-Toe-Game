6c10b56394229c7631a6db57675ef7a00e5c082b
MayyarJavaProgrammer <mayarzidane1@gmail.com>
2022-06-13 15:36:49 +0300

    EDIT palyerClass (ADD playerIcon/colorIcon & make members private)

--- src/tictactoegame/Player.java
+++ src/tictactoegame/Player.java
@@ -4,47 +4,72 @@
  */
 package tictactoegame;
 
+import java.awt.Color;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author Mayyar
  */
 public class Player {
-
-    int score;
-    String name;
-    boolean isPlayerTurn;
-
+    
+    private int score;
+    private String name;
+    private boolean isPlayerTurn;
+    private char playerIcon;
+    private Color colorIcon;
+    
     public Player() {
         
     }
-
-    public Player(int score, String name, boolean isPlayerTurn) {
+    
+    public Player(int score, String name, boolean isPlayerTurn, char playerIcon) {
         this.score = score;
         this.name = name;
         this.isPlayerTurn = isPlayerTurn;
+        this.playerIcon = playerIcon;
     }
-
+    
     public boolean isIsPlayerTurn() {
         return isPlayerTurn;
     }
-
+    
     public void setIsPlayerTurn(boolean isPlayerTurn) {
         this.isPlayerTurn = isPlayerTurn;
     }
-
+    
     public void setScore(int score) {
         this.score = score;
     }
-
+    
     public int getScore() {
         return score;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
+    
+    public char getPlayerIcon() {
+        return playerIcon;
+    }
+    
+    public void setPlayerIcon(char playerIcon) {
+        this.playerIcon = playerIcon;
+        
+        if (playerIcon == 'x') {
+            setColorIcon(Color.BLUE);
+        } else if (playerIcon == 'o') {
+            setColorIcon(Color.RED);
+        }
+    }
+    
+    public void setColorIcon(Color colorIcon) {
+        this.colorIcon = colorIcon;
+    }
+    
 }
